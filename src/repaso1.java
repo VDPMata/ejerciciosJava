e66b233600c4c9deb41df3d3c2d1dc0ed83562f9
VDPMata <vdpmata@gmail.com>
2023-07-04 18:32:51 -0300

    agregado ignore

---
+++ src/repaso1.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class repaso1 {
+    public static void main(String[] args) {
+        //fibo();
+        numPrimos();
+    }
+
+    // Fibonacci
+    // Escribir un programa que imprima los primeros 8 términos de la serie de
+    // Fibonacci utilizando un bucle while. La serie de Fibonacci es una secuencia
+    // donde cada número es la suma de los dos anteriores.
+    // La serie de Fibonacci comienza de la siguiente manera:
+    // Posición 0: 0
+    // Posición 1: 1
+    // Posición 2: 1 (suma de la posición 0 y la posición 1, 0 + 1 = 1)
+    // Posición 3: 2 (suma de la posición 1 y la posición 2, 1 + 1 = 2)
+    // Posición 4: 3 (suma de la posición 2 y la posición 3, 1 + 2 = 3)
+
+    // Entonces, los primeros 5 términos (considerando el índice inicial 0) de la
+    // serie de Fibonacci son: 0, 1, 1, 2, 3.
+    public static void fibo(){
+        int contador = 8;
+        int posicion = 0;
+        int indiceActual = 0;
+        int indiceAnterior = 1;
+
+        while(posicion < contador){
+            int indiceSiguiente = indiceActual + indiceAnterior;
+            System.out.println("Posicion " + posicion + ": " + indiceActual);
+            indiceAnterior = indiceActual;
+            indiceActual = indiceSiguiente;
+            posicion++;
+        }
+    }
+
+    // Números primos
+    // Solicitar al usuario que introduzca un número y verificar si es primo
+    // utilizando un bucle do-while.  
+    // Un número es primo si solo tiene dos divisores: 1 y él mismo. 
+    // Si el número no es primo, pedir al usuario que introduzca otro número hasta
+    // que introduzca un número primo.
+    public static void numPrimos(){
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+
+        do {
+            System.out.print("Ingrese un número: ");
+            numero = scanner.nextInt();
+        } while (!esPrimo(numero));
+
+        System.out.println(numero + " es un número primo.");
+        scanner.close();
+    }
+
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
+    // Factorial
+    // Escribir un programa que solicite al usuario un número y calcule su factorial
+    // utilizando un bucle do-while.
+    // 💡 El factorial de un número es el producto de todos los números desde 1
+    // hasta ese número. Te compartimos algunos ejemplos:
+    // El factorial de 3 es 1 * 2 * 3 = 6.
+    // El factorial de 5 es 1 * 2 * 3 * 4 * 5 = 120.
+    // El factorial de 7 es 1 * 2 * 3 * 4 * 5 * 6 * 7 = 5040.
+    public static void factorial(){
+
+    }
+    // Fibonacci plus
+    // Basándote en el primer ejercicio, ahora crear un programa que solicite al
+    // usuario ingresar la posición de un número en la serie de Fibonacci y mostrar
+    // el valor correspondiente a esa posición.
+
+    // Recuerda que la serie de Fibonacci comienza con los siguientes valores:
+    // Posición 0: 0
+    // Posición 1: 1
+    // Posición 2: 1
+    // Posición 3: 2
+    // Posición 4: 3
+    public static void fiboPlus(){
+
+    }
+
+
+    // Números primos plus
+    // Siguiendo el ejemplo del tercer ejercicio, el desafío ahora es desarrollar un
+    // programa que solicite al usuario la cantidad deseada de números primos a
+    // obtener.
+    // El programa debe generar y mostrar la cantidad de números primos solicitada
+    // por el usuario.
+    public static void numPrimosPlus(){
+
+    }
+}
